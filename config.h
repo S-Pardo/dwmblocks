1d4ff05474788e3908fd8aee2a3c5e3d6f821ec7
Sebastian Pardo <sapardosuazo@gmail.com>
2020-04-05 18:02:00 -0400

    Añade bloque de memoria y elina los no utilizados

--- config.h
+++ config.h
@@ -2,15 +2,11 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	/* {"",	"crypto",	0,	13}, */
-	{"",	"torrent",	20,	7},
+	{"",	"pacpackages",	18000,	8},
 	{"",	"news",		0,	6},
-	{"",	"disk /dev/sda4 💻",	 	0,	0},
-	/* {"",	"moonphase",	18000,	5}, */
 	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
+	{"",	"memory",	40,	10},
+	{"",	"disk /dev/sda4 💻",	 	0,	0},
 	{"",	"volume",	0,	10},
 	{"",	"battery | tr \'\n\' \' \'",	5,	0},
 	{"",	"clock",	60,	0},
